de3ec8cfd65a60394838dd3def707a8c188feac0
Chandana Robba <chandana.robba@gmail.com>
2026-02-23 16:35:50 -0800

    setting up project with spring boot initializr

---
+++ converge/src/test/java/com/converge/ConvergeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.converge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvergeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
