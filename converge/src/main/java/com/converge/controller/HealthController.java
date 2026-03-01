b56a9d0652fd314657f21ea4dab22484fabf57f8
Chandana Robba <chandana.robba@gmail.com>
2026-02-28 21:59:24 -0800

    update comment for better documentation

--- converge/src/main/java/com/converge/controller/HealthController.java
+++ converge/src/main/java/com/converge/controller/HealthController.java
@@ -3,6 +3,7 @@ package com.converge.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//health check to check server status (GET)
 @RestController
 public class HealthController {
 
